70f4223423c2442b1d285f13ef251a2c61408c4d
Xma1 <xma1@mail.ru>
2015-10-11 01:51:40 +0200

    [impl] Face Detection Capture

--- FaceDetection/FaceDetection/Form1.Designer.cs
+++ FaceDetection/FaceDetection/Form1.Designer.cs
@@ -32,7 +32,10 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.LoadBTN = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.captureButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // startBTN
@@ -49,7 +52,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 41);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(529, 491);
+            this.pictureBox1.Size = new System.Drawing.Size(393, 491);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -68,17 +71,39 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(174, 12);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(89, 23);
+            this.captureButton.TabIndex = 4;
+            this.captureButton.Text = "Start Capture";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(411, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(428, 491);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(553, 544);
+            this.ClientSize = new System.Drawing.Size(851, 544);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.captureButton);
             this.Controls.Add(this.LoadBTN);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.startBTN);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -89,6 +114,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button LoadBTN;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button captureButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- FaceDetection/FaceDetection/Form1.cs
+++ FaceDetection/FaceDetection/Form1.cs
@@ -12,21 +12,66 @@ using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.CV.UI;
 using Emgu.CV.Cuda;
+using Emgu.Util;
 
 namespace FaceDetection
 {
     public partial class Form1 : Form
     {
         string fileName = "lena.jpg";
+        private Capture _capture = null;
+        private bool _captureInProgress;
 
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                _capture = new Capture();
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            try
+            {
+                Mat frame = new Mat();
+                _capture.Retrieve(frame, 0);
+                Mat grayFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, ColorConversion.Bgr2Gray);
+                Mat smallGrayFrame = new Mat();
+                CvInvoke.PyrDown(grayFrame, smallGrayFrame);
+                Mat smoothedGrayFrame = new Mat();
+                CvInvoke.PyrUp(smallGrayFrame, smoothedGrayFrame);
+
+                //Image<Gray, Byte> smallGrayFrame = grayFrame.PyrDown();
+                //Image<Gray, Byte> smoothedGrayFrame = smallGrayFrame.PyrUp();
+                Mat cannyFrame = new Mat();
+                CvInvoke.Canny(smoothedGrayFrame, cannyFrame, 100, 60);
+
+                //Image<Gray, Byte> cannyFrame = smoothedGrayFrame.Canny(100, 60);
+
+                pictureBox2.Image = frame.Bitmap;
+                DetectFace(frame);
+            }
+            catch (Exception ex)
+            { }
         }
 
         private void startBTN_Click(object sender, EventArgs e)
         {
             Mat image = new Mat(fileName, LoadImageType.Color); //Read the files as an 8-bit Bgr image  
+            DetectFace(image);
+        }
+
+        private void DetectFace(Mat image)
+        {
             long detectionTime = 0;
             List<Rectangle> faces = new List<Rectangle>();
             List<Rectangle> eyes = new List<Rectangle>();
@@ -34,7 +79,7 @@ namespace FaceDetection
             bool tryUseCuda = false;
             bool tryUseOpenCL = true;
 
-            DetectFace.Detect(
+            FaceDetection.DetectFace.Detect(
             image, "haarcascade_frontalface_default.xml", "haarcascade_eye.xml",
             faces, eyes,
             tryUseCuda,
@@ -61,6 +106,26 @@ namespace FaceDetection
                 {
                     MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
                 }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    captureButton.Text = "Start Capture";
+                    _capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    captureButton.Text = "Stop";
+                    _capture.Start();
+                }
+
+                _captureInProgress = !_captureInProgress;
             }
         }
     }
