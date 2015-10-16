cc78ebbc42521ef7d7bfb80ab02594242dedcaa9
Xma1 <xma1@mail.ru>
2015-10-16 21:49:48 +0200

    [impl] open YUV

--- Codek Tester/Codek Tester/Form1.Designer.cs
+++ Codek Tester/Codek Tester/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // LoadBTN
@@ -61,16 +65,36 @@
             this.button1.Text = "Decode";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(352, 288);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(370, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(352, 288);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(257, 237);
+            this.ClientSize = new System.Drawing.Size(734, 387);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.LoadBTN);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -81,6 +105,8 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Codek Tester/Codek Tester/Form1.cs
+++ Codek Tester/Codek Tester/Form1.cs
@@ -7,14 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
 
 namespace Codek_Tester
 {
     public partial class Form1 : Form
     {
+        YUV Input;
+        YUV Outout;
         public Form1()
         {
             InitializeComponent();
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                Input = new YUV("mobile_cif.yuv", 352, 288);
+                Input.ImageGrabbed += ProcessFrameInput;
+                Input.Start();
+
+                Outout = new YUV("mobile_out.yuv", 352, 288);
+                Outout.ImageGrabbed += ProcessFrameOutput;
+                Outout.Start();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void ProcessFrameInput(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Mat frame = new Mat();
+                Input.Retrieve(frame, 0);
+                pictureBox1.Image = new Bitmap(frame.Bitmap);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void ProcessFrameOutput(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Mat frame = new Mat();
+                Input.Retrieve(frame, 0);
+                pictureBox2.Image = new Bitmap(frame.Bitmap);
+            }
+            catch (Exception ex)
+            {
+            }
         }
     }
 }
